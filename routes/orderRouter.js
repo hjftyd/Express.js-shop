56029595aad7f3d46141c3c8864c1c1808a01817
Vladislav <63506567+hjftyd@users.noreply.github.com>
2022-06-06 01:26:25 +0600

    Update orderRouter.js

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router
 const orderController = require('../controllers/orderController')
+const checkAuth = require('../middleware/checkAuth')
 
-router.post('/create', orderController.createOrder)
-router.delete('/delete/:id', orderController.deleteOrder)
+router.post('/create', checkAuth, orderController.createOrder)
+router.delete('/delete/:id', checkAuth, orderController.deleteOrder)
 
 module.exports = router
