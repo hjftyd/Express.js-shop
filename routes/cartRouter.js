86200fd5eea9ebff502ad0afbe5d8a283b76ae89
Vladislav <63506567+hjftyd@users.noreply.github.com>
2022-06-06 01:29:15 +0600

    Added missing

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -3,7 +3,6 @@ const router = new Router
 const cartController = require('../controllers/cartController')
 const checkAuth = require('../middleware/checkAuth')
 
-
 router.get('/:id', checkAuth, cartController.getUserCart)
 router.post('/addToCart', checkAuth, cartController.addItemToCart)
 router.delete('/empty/:id', checkAuth, cartController.emptyCart)
